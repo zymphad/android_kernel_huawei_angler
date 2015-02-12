a890e278540386dc7a7c9741d265e975deb9ab0b
franciscofranco <franciscofranco.1990@gmail.com>
2015-02-12 03:51:32 +0000

    wakeup: add userspace knob to enable/disable msm_hsic_host wakelock
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>
    Signed-off-by: engstk <eng.stk@sapo.pt>
    Signed-off-by: scott <scott@ubuntu>
    
    Conflicts:
    	drivers/base/power/wakeup.c

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -28,6 +28,8 @@ static bool enable_wlan_wake_ws = true;
 module_param(enable_wlan_wake_ws, bool, 0644);
 static bool enable_bluedroid_timer_ws = true;
 module_param(enable_bluedroid_timer_ws, bool, 0644);
+static bool enable_msm_hsic_ws = true;
+module_param(enable_msm_hsic_ws, bool, 0644);
 
 /*
  * If set, the suspend/hibernate code will abort transitions to a sleep state
@@ -433,6 +435,9 @@ static void wakeup_source_activate(struct wakeup_source *ws)
 
 	if (!enable_bluedroid_timer_ws && !strcmp(ws->name, "bluedroid_timer"))
 		return;
+
+	if (!enable_msm_hsic_ws && !strcmp(ws->name, "msm_hsic_host"))
+                return;
 
 	/*
 	 * active wakeup source should bring the system
